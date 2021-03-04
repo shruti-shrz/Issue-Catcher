c8c17cf12aaaf6544a07591375fbd4bf785163d4
shruti-shrz <shrutipriyasp18@gmail.com>
2021-03-04 06:30:48 +0530

    Scraper to scrape all issues

--- plugin-client/content.js
+++ plugin-client/content.js
@@ -1,10 +1,34 @@
 console.log("hello");
 console.log(window.location.href);
 var url = window.location.href;
+var flag =0;
+chrome.storage.sync.get('userurl', function(data){
+	console.log(data);
+	flag = 1;
+});
+if(flag==0)
+{
+	chrome.storage.sync.set({'userurl': url}, function(){
+
+		console.log("Saved!!!")
+	});
+	
+}
+
 if(url.includes("issues"))
 {
 	var res = url.split("/");
-	console.log(res[res.length - 1])
+	var l = res[res.length - 1]
+	let isnum = /^\d+$/.test(l);
+	if(isnum)
+	{
+		chrome.storage.sync.set({'userissue': res[res.length - 1]}, function(){
+		console.log("Issue Selected!!!")
+		console.log(res[res.length - 1])
+	});
+	
+	}
+	
 }
 //#issue_47523_link
 // let paragraphs = document.get(".issue");#issue_1_link
