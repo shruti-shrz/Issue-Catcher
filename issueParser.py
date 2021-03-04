1b7f99cb5e665d3b5752e09aa62da74d518ae86d
shruti-shrz <shrutipriyasp18@gmail.com>
2021-03-04 06:38:29 +0530

    Scraper to scrape all issues

--- issueParser.py
+++ issueParser.py
@@ -9,22 +9,23 @@ def issueparser(url_repo):
 	for sol in soup.findAll('div',{'class':'flex-auto'}):
 		k = sol.find('a')
 		if k != None:
-
 			n = k['href']
 			var = n.split('/')
 			if var[-1].isdigit():
-				print(k.text)
+				print("Issue:  "+k.text)
 				print(n)
 				i = i+1
-				url2 = url_repo + var[-1]
+				url2 = url_repo + '/' +var[-1]
 				plain_html_text2 = requests.get(url2)
 				soup2 = BeautifulSoup(plain_html_text2.text, "html.parser")
 				m = soup2.find('td')
+				
 				if m != None:
 					q = m.find('p')
 					if q != None:
 						r = q.text
-						print(r)
+						print("Issue Description:  "+r)
+						
 
 	 	# url2 = "https://www.codechef.com" + l
 	 	# plain_html_text2 = requests.get(url2)
