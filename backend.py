f68b62235231e19940e755514b80199b6080dfd0
Pranathi1413 <cs18b045@iittp.ac.in>
2021-03-23 00:51:22 +0530

    db commands and backend response

--- backend.py
+++ backend.py
@@ -1,30 +1,40 @@
-#import pymongo
-#from pymongo import MongoClient
+import pymongo
+from pymongo import MongoClient
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 from issueParser import *
-#cluster =  MongoClient("")
-#db = cluster["githubRepos"]
-#collection = db["repos_issues"]
+cluster =  MongoClient("")
+db = cluster['IssueDB']
+collection = db['issues']
 k = "hello"
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
 
+@app.route('/', methods=['GET', 'POST'])
 def bootstrap():
-	global k
 	if request.method == 'POST':
 		if not request.form:
 			print("nothing!!!")
+			return jsonify({'error' : 'Empty request'})
 		else:
 			var = request.form
 			k = var['issue']
 			print(k,type(k))
-			get_ans(k)
-			#myquery = {"issue_number":"test"}
-			#myissue = collection.find(myquery)
+			myquery = {"url":k}
+			myissue = None
+			myissue = collection.find(myquery)
+			ans = []
+			if myissue == None:
+				ans = get_ans(k)
+				i = {'url' : k, 'ans': ans}
+				collection.insert_one(i)
+			else:
+				ans = myissue['ans']
+			if len(ans) > 0:
+				return jsonify(ans)
+			else:
+				return jsonify({'error':'Similar Issues Not Found'})
 			#for x in myissue:
 			#	print(x)
-	return k
 
 if __name__ == "__main__":
     app.run(debug=True)
