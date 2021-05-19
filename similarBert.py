2e4b52c9df9630bcd51cd629b2993ae3c553c22b
Pranathi <cs18b045@iittp.ac.in>
2021-05-20 01:52:06 +0530

    For deployment

--- similarBert.py
+++ similarBert.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-# import nltk
-# nltk.download('stopwords')
+#import nltk
+#nltk.download('stopwords')
 from nltk.corpus import stopwords
 import nltk
 import re
